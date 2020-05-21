001401227fef6dedb079ab65840c4debd3ae4a7f
hristodinev <arm_104@abv.bg>
2020-05-21 17:41:46 +0300

    Add Pages Controller, add vuejs to the project and include header as partial in the layout file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -14,32 +14,11 @@
 <body>
     <div class="container">
 
-        <header>
-            <div class="header-nav">
-                <div class="header-nav-logo">
-                    <a href="#">
-                        <img src="/images/header_logo.png" alt="The logo" srcset="">
-                    <span class="logо">МЦ Д-р Диневи</span>
-                    </a>
-                </div>
-            
-                <div class="header-nav-menu">
-                    <ul>
-                        <li><a class = "nav_menu_items" href=""></a>НАЧАЛО</li>
-                        <li><a class = "nav_menu_items" href=""></a>ЗА НАС</li>
-                        <li><a class = "nav_menu_items" href=""></a>СПЕЦИАЛИСТИ</li>
-                        <li><a class = "nav_menu_items" href=""></a>УСЛУГИ</li>
-                        <li><a class = "nav_menu_items" href=""></a>КОНТАКТИ</li>
-                    </ul>
-                </div>
-
-            </div> 
-          
-        </header>
-
+       @include('partials.header')
         @yield('content')
 
 
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 </body>
 </html>
